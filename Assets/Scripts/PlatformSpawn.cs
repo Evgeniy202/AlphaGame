08b2658f19f3a2b79bbde0bb5bd9dcffcfcc0325
Evgeniy202 <evgeniykrotko.2002@gmail.com>
2022-01-20 21:56:12 +0200

    Add glow

--- Assets/Scripts/PlatformSpawn.cs
+++ Assets/Scripts/PlatformSpawn.cs
@@ -11,7 +11,7 @@ public class PlatformSpawn : MonoBehaviour
         for (int i = 0; i < 30; i++)
         {
             SpawnerPosition.x = Random.Range(-10f, 10f);
-            SpawnerPosition.y = Random.Range(1f, 50f);
+            SpawnerPosition.y = Random.Range(3f, 50f);
             SpawnerPosition.z = 15f;
 
             Instantiate(newPlatform, SpawnerPosition, Quaternion.identity);
